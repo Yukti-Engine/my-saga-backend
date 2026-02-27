d7b603bbd10b41400dac6ce31cb5bc2127a604b8
VivekBengani <vivek@beatly.ai>
2026-02-27 13:32:44 +0530

    upload and download files

--- src/controllers/adventureController.ts
+++ src/controllers/adventureController.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
 import pool from "../dbms/db.js";
-import { isRelatedToAdventure, countMessages, getMessagesFromAToB, roomAvailable} from '../dbms/adventure-helpers.js';
+import { isRelatedToAdventure, countMessages, getMessagesFromAToB, roomAvailable, fileCount} from '../dbms/adventure-helpers.js';
 import {getBoss} from '../dbms/boss-helpers.js';
 import {getUser} from '../dbms/user-helpers.js';
 import { getOrganizer } from "../dbms/organizer-helpers.js";
+import { generateDownloadUrl, generateUploadUrl } from "../services/bucketService.js";
 
 export const count = async (req: Request, res: Response) => {
   const { adventureId, id, role, accessToken} = req.body;
@@ -46,7 +47,7 @@ export const getMessages = async (req: Request, res: Response) => {
     else
       return res.status(500).json({"error": "Access token does not match"});
   else
-    return res.status(500).json({"error": "No such organizer"});
+    return res.status(500).json({"error": "No such person"});
 }
 
 export default function roomSocket(io:any, socket:any) {
@@ -69,3 +70,56 @@ export default function roomSocket(io:any, socket:any) {
     socket.to(roomName).emit("message", "A user has left!");
   });
 }
+
+export async function  getUploadFileUrl(req:any, res:any)  {
+  const { fileName, contentType, adventureId, id, role, accessToken } = req.body;
+  let person;
+  if (role == "organizer")
+    person = await getOrganizer(id, pool);
+  else if (role == "boss")
+    person = await getBoss(id, pool);
+  else
+    person = await getUser(id, pool);
+  if (person)
+    if (person.access_token == accessToken && accessToken)
+    {
+      if (await isRelatedToAdventure(id, role, adventureId, pool)) {
+        const data = await generateUploadUrl(fileName, contentType, adventureId, await fileCount(adventureId, pool));
+
+        res.json(data);
+      }
+      return res.json({success:false});
+    }
+    else
+      return res.status(500).json({"error": "Access token does not match"});
+  else
+    return res.status(500).json({"error": "No such person"});
+}
+
+export async function getDownloadFileUrl(req:any, res:any) {
+  const { fileName, adventureId, fileNumber, id, role, accessToken } = req.body;
+    let person;
+    if (role == "organizer")
+      person = await getOrganizer(id, pool);
+    else if (role == "boss")
+      person = await getBoss(id, pool);
+    else
+      person = await getUser(id, pool);
+    if (person)
+      if (person.access_token == accessToken && accessToken)
+      {
+        if (await isRelatedToAdventure(id, role, adventureId, pool)) {
+          const data = await generateDownloadUrl(fileName, adventureId, fileNumber);
+          return res.json({ data });
+        }
+        return res.json({success:false});
+      }
+      else
+        return res.status(500).json({"error": "Access token does not match"});
+    else
+      return res.status(500).json({"error": "No such person"});
+}
+  
+  
+
+
