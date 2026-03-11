a26c8d70b01515bba81d2410bd25065184cc9c34
VivekBengani <vivek@beatly.ai>
2026-03-11 21:14:09 +0530

    bosss dashboard made complete?

--- src/controllers/bossController.ts
+++ src/controllers/bossController.ts
@@ -75,7 +75,7 @@ export const getBossDashboard = async (req: Request, res: Response) => {
   const boss = await getBoss(bid, pool);
   if (boss)
     if (boss.access_token == accessToken && accessToken)
-      return res.json({"username":boss.username, "gender":boss.gender, "bio":boss.bio, "dob":boss.dob, "setting_1":boss.setting_1, "setting_2": boss.setting_2, "icon":boss?.icon.toString("base64")});
+      return res.json({"username":boss.username, "gender":boss.gender, "bio":boss.bio, "dob":boss.dob, "setting_1":boss.setting_1, "setting_2": boss.setting_2, "icon":boss.icon?.toString("base64")});
     else
       return res.status(500).json({"error": "Access token does not match"});
   else
