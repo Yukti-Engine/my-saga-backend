51c05566250c524d89f449dcda65dde2e98e11b5
VivekBengani <vivek@beatly.ai>
2026-03-18 00:55:36 +0530

    getProfile

--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCategories, getSubcategories, getOffers, generateAdventureName, findLobbies, getBadges } from "../controllers/searchController.js";
+import { getCategories, getSubcategories, getOffers, generateAdventureName, findLobbies, getBadges, getProfile } from "../controllers/searchController.js";
 
 const router = express.Router();
 router.post("/lobbies", findLobbies);
@@ -8,4 +8,5 @@ router.post("/subcategories", getSubcategories);
 router.post("/offers", getOffers);
 router.post("/badges", getBadges);
 router.post("/random-adventure-name", generateAdventureName);
+router.post("/profile", getProfile);
 export default router;
